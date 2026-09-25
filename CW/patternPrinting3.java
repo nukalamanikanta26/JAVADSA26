5e69bbb1013a4ba3b1359e0c22654d8ce82c8ce2
manikantanukala26 <22221a0582@bvcgroup.in>
2026-09-25 11:21:40 +0530

    Print Pattern 3

---
+++ CW/patternPrinting3.java
@@ -0,0 +1,16 @@
+public class patternPrinting3 {
+    public static void main(String[] args) {
+        patternPrinting(5);
+    }
+
+    static void patternPrinting(int n) {
+        int sum =1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(sum + " ");
+                sum++;
+            }
+            System.out.println();
+        }
+    }
+}
